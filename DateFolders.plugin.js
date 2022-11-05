0415efd721c0ee4dce48fe37ec23618e202d82d4
JeLuF <jf@mormo.org>
2022-11-05 19:52:57 +0100

    DateFolders.plugin.js

---
+++ DateFolders.plugin.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         DateFolders
+// @version      1.0
+// @description  Use human readable timestamps as session IDs instead of microseconds since 1970-01-01
+// @author       JeLuF
+
+
+DateFoldersDate= new Date();
+sessionId = d.toISOString() + Date.now();
